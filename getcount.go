32f9f2d0e47651310a457e81395f971aa918a58f
Mikołaj Gąsior <m@gasior.dev>
2026-03-06 21:20:07 +0100

    Add ConvertFiltersFromString to GetCount

--- getcount.go
+++ getcount.go
@@ -3,11 +3,14 @@ package crud
 import (
 	"context"
 
+	sqlbuilder "miko.gs/pgsql-builder"
 	sqlfilters "miko.gs/pgsql-builder/pkg/filters"
+	validator "miko.gs/struct-validator"
 )
 
 type GetCountOptions struct {
-	Filters *sqlfilters.Filters
+	Filters                  *sqlfilters.Filters
+	ConvertFiltersFromString bool
 }
 
 // GetCount runs a 'SELECT COUNT(*)' query on the database with specified filters, order, limit and offset and returns count of rows
@@ -15,6 +18,39 @@ func (c *CRUD) GetCount(ctx context.Context, obj interface{}, options GetCountOp
 	builder, err := c.builder(obj)
 	if err != nil {
 		return 0, getBuilderObjectCRUDError(err)
+	}
+
+	// Filter values can be passed as string. We do not want any use of reflect outside of CRUD.
+	if options.ConvertFiltersFromString {
+		newFilters := &sqlfilters.Filters{}
+
+		for filterName, filterOpVal := range *(options.Filters) {
+			// ignore the raw filters entirely, that's too complicated
+			if filterName == sqlfilters.Raw {
+				continue
+			}
+
+			valueAsString, ok := filterOpVal.Val.(string)
+			if !ok {
+				return 0, getObjInvalidCRUDError(map[string]int{
+					filterName: validator.FailType,
+				})
+			}
+
+			ok, valueAsFieldType := sqlbuilder.StructFieldValueFromString(obj, filterName, valueAsString)
+			if !ok {
+				return 0, getObjInvalidCRUDError(map[string]int{
+					filterName: validator.FailType,
+				})
+			}
+
+			newFilters.Add(filterName, sqlfilters.OpVal{
+				Op:  filterOpVal.Op,
+				Val: valueAsFieldType,
+			})
+		}
+
+		options.Filters = newFilters
 	}
 
 	err = ValidateFilters(obj, options.Filters, c.tagName)
